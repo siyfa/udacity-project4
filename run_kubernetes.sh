1320f3395e3b829b7891ee51d5f7b099b4864bc1
Ubuntu <ubuntu@ip-172-31-14-27.ec2.internal>
2022-09-13 15:15:39 +0000

    roundup project

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,10 +9,19 @@ dockerpath=siyfa/udacity-project4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run udacity-project4 \
+r=$(kubectl get pod udacity-project4 2> /dev/null;echo $?)
+
+if [ "_$r" == "_1" ] ; then
+    kubectl run udacity-project4 \
     --image=$dockerpath \
     --image-pull-policy="Always" \
     --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
+fi
+
+# kubectl run udacity-project4 \
+#     --image=$dockerpath \
+#     --image-pull-policy="Always" \
+#     --overrides='{"apiVersion": "v1", "spec":{"imagePullSecrets": [{"name": "regcred"}]}}'
 
 # Step 3:
 # List kubernetes pods
